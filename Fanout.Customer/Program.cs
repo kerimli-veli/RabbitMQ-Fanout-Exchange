0e60de67c80ca925907f7843f16d20952b884951
kerimli-veli <kerimovveli001@gmail.com>
2025-05-09 17:43:30 +0400

    Add project files.

---
+++ Fanout.Customer/Program.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+Console.WriteLine("Consumer");
+
+ConnectionFactory factory = new ConnectionFactory
+{
+    Uri = new Uri("amqps://fpuoiyor:t8SZYf8YmxPfSnvxwPiWbKYJwjfmNWlW@goose.rmq2.cloudamqp.com/fpuoiyor")
+};
+
+using IConnection connection = await factory.CreateConnectionAsync();
+using IChannel channel = await connection.CreateChannelAsync();
+
+
+string exchangeName = "fanaut-example - with - my-group";
+await channel.ExchangeDeclareAsync(exchange: exchangeName, type: ExchangeType.Fanout);
+
+
+QueueDeclareOk queue = await channel.QueueDeclareAsync(queue: "", exclusive: true);
+
+
+await channel.QueueBindAsync(queue: queue.QueueName, exchange: exchangeName, routingKey: "");
+
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+consumer.ReceivedAsync += async (model, ea) =>
+{
+    byte[] body = ea.Body.ToArray();
+    string message = Encoding.UTF8.GetString(body);
+    Console.WriteLine("Gelen mesaj: " + message);
+    await Task.Yield(); 
+};
+
+await channel.BasicConsumeAsync(queue: queue.QueueName, autoAck: true, consumer: consumer);
+
+
+Console.WriteLine("Mesajlar gözlənilir...");
+Console.ReadLine();
---
+++ Fanout.Publiser/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using System.Text;
+
+Console.WriteLine("Publiser");
+
+
+
+ConnectionFactory factory = new ConnectionFactory();
+
+factory.Uri = new("amqps://fpuoiyor:t8SZYf8YmxPfSnvxwPiWbKYJwjfmNWlW@goose.rmq2.cloudamqp.com/fpuoiyor");
+
+using IConnection connection = await factory.CreateConnectionAsync();
+
+using IChannel channel = await connection.CreateChannelAsync();
+
+
+
+await channel.ExchangeDeclareAsync(exchange: "fanaut-example - with - my-group", type: ExchangeType.Fanout);
+
+for(int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes($"number {i}");
+    await channel.BasicPublishAsync(
+         exchange: "fanaut-example - with - my-group",
+         routingKey: string.Empty,
+         body: message
+
+        );
+
+}
